b871953c53195ef5992dd697d55d12a143e3472d
= <=>
2025-10-08 10:46:36 +0100

    changed post button onclick event

--- public/scripts/dialogActions.js
+++ public/scripts/dialogActions.js
@@ -1,10 +1,6 @@
 const dialog = document.querySelector("dialog");
 const openButton = document.querySelector("#post-global");
 const closeButton = document.querySelector("#close-btn");
-openButton.addEventListener("click", () => {
-    console.log('clicked');
-    dialog.showModal();
-});
 
 closeButton.addEventListener("click", () => {
     document.getElementById("text-area-post").value = '';
